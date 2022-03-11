364d73b7d9abfa661428f7e0f61ae42f17e5ddbd
000quanbi000 <101376866+000quanbi000@users.noreply.github.com>
2022-03-11 09:58:00 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
     titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Hà My bét blitz',
+    introDesc: `chào người eo nha`,
+    btnIntro: 'henhu',
+    title: 'biết là mình thích nhau',
+    desc: 'Còn chuyện xa hơn thôi để tính sau ',
+    btnYes: 'Thích ',
+    btnNo: 'Không thích',
+    question: 'Iu ăng hong',
+    btnReply: 'adu babi gwrl à',
+    reply: 'Yêu',
+    mess: 'anh biết mà thankiu babigwrl ',
+    messDesc: 'khi nào 1 vạch đi chơi nha đồ hâm ',
+    btnAccept: 'dạ anh',
+    messLink: 'https://www.facebook.com/profile.php?id=100046817472601' 
 }
